2ee6816c02cb236abb228348e0d5f95cc6d12c59
Ramesh Pathak <Ramesh.Pathak@sapiens.com>
2022-06-26 23:12:56 +0530

    Added Filters

--- SpringBoot_CRUD/src/main/java/com/springboot/crud/controller/EmployeeController.java
+++ SpringBoot_CRUD/src/main/java/com/springboot/crud/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.springboot.crud.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,6 +21,7 @@ import com.springboot.crud.service.EmployeeService;
 @RestController
 @RequestMapping("/api/c1")
 public class EmployeeController {
+	Logger logger= LoggerFactory.getLogger(EmployeeController.class);
 	
 	@Autowired
 	EmployeeService employeeService;
@@ -26,6 +29,7 @@ public class EmployeeController {
 	@GetMapping("/allEmployees")
 	public ResponseEntity<List<Employee>> getAllEmployee() {
 		/* return employeeService.getEmployees(); */
+		logger.info("Inside the getAllEmployee()" );
 		return  ResponseEntity.ok(employeeService.getEmployees());
 	}
 	
